4253a083002f2462462cb2751e4ae815d824cc3b
keita <banira0428@gmail.com>
2020-04-11 13:50:35 +0900

    CSV 生成用関数の作成

--- functions/src/testToText.ts
+++ functions/src/testToText.ts
@@ -1,5 +1,10 @@
 import {Test} from './Test'
+import * as functions from "firebase-functions";
 
-export const generateCSV = function (test: Test) {
+export const testToText = functions.https.onRequest((req, res) => {
+    res.status(200).send(generateCSV(req.body));
+});
 
+const generateCSV = function (test: Test) {
+    return {text: ""}
 };
